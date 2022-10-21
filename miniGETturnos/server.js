b32eaa51c75e2e7d90a691daa674a12d868d1d2b
Silverado42 <43814714+Silverado42@users.noreply.github.com>
2022-10-21 12:26:49 -0300

    Modificado para que acepté CORS

--- miniGETturnos/server.js
+++ miniGETturnos/server.js
@@ -3,12 +3,14 @@ const Turnos = require('./model/turnosModel')
 
 
 const server = http.createServer((req, res) => {
+
     if (req.url === '/api/turnos' && req.method === 'GET') {
       getTurnos(req, res);
     }
     else if(req.url === '/api/turnos' && req.method === 'POST'){
         createTurno(req,res)
     }
+
 });
 
 const PORT = process.env.PORT || 5000;
@@ -24,7 +26,13 @@ async function getTurnos(req, res) {
     try {
         const turnos = await Turnos.findAll()
 
-        res.writeHead(200, { 'Content-Type': 'application/json' })
+        const headers = {
+            'Access-Control-Allow-Origin': '*', /* @dev First, read about security */
+            'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+            'Access-Control-Max-Age': 2592000, // 30 days
+            'Content-Type': 'application/json'
+        }
+        res.writeHead(200, headers);
         res.end(JSON.stringify(turnos))
     } catch (error) {
         console.log(error)
@@ -45,7 +53,13 @@ async function createTurno(req, res) {
 
         const newTurno = await Turnos.create(turno)
 
-        res.writeHead(201, {'Content-Type': 'application/json'})
+        const headers = {
+            'Access-Control-Allow-Origin': '*', /* @dev First, read about security */
+            'Access-Control-Allow-Methods': 'OPTIONS, POST, GET',
+            'Access-Control-Max-Age': 2592000, // 30 days
+            'Content-Type': 'application/json'
+        }
+        res.writeHead(201, headers);
         return res.end(JSON.stringify(newTurno))
     } catch (error) {
         console.log(error)

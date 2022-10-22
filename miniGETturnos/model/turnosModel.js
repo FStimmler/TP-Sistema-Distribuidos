13930cb7f87c3535494f692cf3de7cbf7edab615
camilaezama <camilaezama2000@gmail.com>
2022-10-22 11:40:56 -0300

    prueba filter
    prueba filter

--- miniGETturnos/model/turnosModel.js
+++ miniGETturnos/model/turnosModel.js
@@ -18,45 +18,68 @@ function create(turno){
         
             const map = new Map(Object.entries(JSON.parse(data)));
         
-            var ultturno = map.get((map.size-1).toString());
-
-            map.filter((userId) => userId == turno.userId)
-
-            console.log(map)
+            //var ultturno = map.get((map.size-1).toString());
 
             
-            console.log(ultturno["userId"])
+
+            const id3 = map.filter(turno => {        
+                return true;
+            }).map(turno => {
+                return turno;
+            });
+
+            // map.filter((userId) => userId == turno.userId)
+
+            console.log("*************************************")
+            console.log(id3);
+
+            
+            //console.log(ultturno["userId"])
             // console.log(ultturno);
             //console.log(ultturno["id"]-1);
         
-            if (ultturno != null){
-                var turno = {
-                    id: ultturno["id"]+1,
-                    datetime: '2022-09-02T19:58:10.406Z',
-                    userId: ultturno["userId"]+1,
-                    email: 'email@gmail.com',
-                    branchId: 4
-                };
+            // if (ultturno != null){
+            //     var turno = {
+            //         id: ultturno["id"]+1,
+            //         datetime: '2022-09-02T19:58:10.406Z',
+            //         userId: ultturno["userId"]+1,
+            //         email: 'email@gmail.com',
+            //         branchId: 4
+            //     };
         
-            } else{
-                var turno = {
-                    id: 1,
-                    datetime: '2022-09-02T19:58:10.406Z',
-                    userId: 3,
-                    email: 'email@gmail.com',
-                    branchId: 4
-                };
-            }
+            // } else{
+            //     var turno = {
+            //         id: 1,
+            //         datetime: '2022-09-02T19:58:10.406Z',
+            //         userId: 3,
+            //         email: 'email@gmail.com',
+            //         branchId: 4
+            //     };
+            // }
+
+            // const mapa = Object.entries(JSON.parse(data)).map(function(element){
+            //     return element;
+            // });        
+        
+            //console.log(mapa);           
+            
+            // let algunos = mapa.filter(turno => {        
+            //     return turno[1]["id"]==1;
+            // }).map(turno => {
+            //     return turno;
+            // });             
+            
+            // console.log(algunos);
         
             //turnos.push(turno);
         
         
-                fs.writeFile("turnos.json", JSON.stringify(turnos), err => {
+                // fs.writeFile("turnos.json", JSON.stringify(turnos), err => {
         
-                if (err) throw err; 
+                // if (err) throw err; 
         
-                console.log(turnos);
-                });
+                // console.log(turnos);
+                // });
         });
     })
 }

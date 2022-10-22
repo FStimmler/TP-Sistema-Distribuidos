44a692970eddebaf4530bcd781038d06a1c1c38a
Silverado42 <43814714+Silverado42@users.noreply.github.com>
2022-10-22 12:58:18 -0300

    Añadido metodo getPostData

--- miniGETturnos/utils.js
+++ miniGETturnos/utils.js
@@ -7,6 +7,25 @@ function writeDataToFile(filename, content){
     })
 }
 
+function getPostData(req) {
+    return new Promise((resolve, reject) => {
+        try {
+            let body = ''
+
+            req.on('data', (chunk) => {
+                body += chunk.toString()
+            })
+
+            req.on('end', () => {
+                resolve(body)
+            })
+        } catch (error) {
+            reject(error)
+        }
+    })
+}
+
 module.exports = {
-    writeDataToFile
+    writeDataToFile,
+    getPostData
 }

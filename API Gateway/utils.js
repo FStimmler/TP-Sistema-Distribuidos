6ecc7ae9b81b94428ecd2e673b3a9f73f7619730
Silverado42 <43814714+Silverado42@users.noreply.github.com>
2022-10-31 12:43:44 -0300

    Borrado métodos no necesarios

--- API Gateway/utils.js
+++ API Gateway/utils.js
@@ -1,12 +1,5 @@
 const fs = require('fs')
 
-function writeDataToFile(filename, content){
-    fs.writeFileSync(filename, JSON.stringify(content), 'utf8', (err) =>{
-        if(err)
-            console.log(err)
-    })
-}
-
 function getPostData(req) {
     return new Promise((resolve, reject) => {
         try {
@@ -25,17 +18,7 @@ function getPostData(req) {
     })
 }
 
-function hashCode(str) {
-    let hash = 0;
-    for (let i = 0, len = str.length; i < len; i++) {
-        let chr = str.charCodeAt(i);
-        hash = (hash << 5) - hash + chr;
-    }
-    return hash;
-}
 
 module.exports = {
-    writeDataToFile,
     getPostData,
-    hashCode
 }
